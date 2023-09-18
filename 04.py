cdb89c44bf7500d5ccf6229803f6197013b215ff
LY <1253300251@qq.com>
2023-09-18 11:54:42 +0800

    Draw range

--- 04.py
+++ 04.py
@@ -2,11 +2,11 @@
 import cv2 as cv
 
 # 读取图片
-img = cv.imread("text1.jpg")
+img = cv.imread("text21.jpg")
 cv.imshow("read", img)
 
 # 坐标(x:起点到图像最左边的距离，y:起点到图像上边距离，w:起点向右走的距离，h:起点向下走的距离，r:圆的半径)
-x, y, w, h = 100, 100, 100, 100
+x, y, w, h = 100, 200, 10, 100
 # 绘制
 cv.rectangle(img, (x, y, w, h), color=(0, 0, 255), thickness=1)
 cv.circle(img, center=(x, y), radius=100, color=(0, 255, 0), thickness=2)
