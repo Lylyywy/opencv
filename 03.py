fa0df54a8c792f699346d81fc07e469aaf18f986
LY <1253300251@qq.com>
2023-09-18 11:46:02 +0800

    resize

--- 03.py
+++ 03.py
@@ -2,19 +2,19 @@
 import cv2 as cv
 
 # 读取图片
-img = cv.imread("text1.jpg")
-cv.imshow("read", img)
-# 修改尺寸
-resize_img = cv.resize(img, dsize=(200, 200))
-# 显示原图
-cv.imshow("resize", resize_img)
-# 打印尺寸大小
+img = cv.imread("text21.jpg")
 print("未修改", img.shape)
-print("修改后", resize_img.shape)
+cv.imshow("read", img)
+h, w = img.shape[:2]
 
-# 等待(按q退出)
-while True:
-    if ord("q") == cv.waitKey(0):
-        break
+resize_img = cv.resize(img, dsize=(200, 200))       #修改尺寸大小为（200,200）
+resize_img1 = cv.resize(img, dsize=(w//4, h//4))    #缩小为原来的1/4
+# 显示
+cv.imshow("resize", resize_img1)
+# 打印尺寸大小
+print("修改后", resize_img1.shape)
+
+# 等待
+cv.waitKey(0)
 # 释放内存
 cv.destroyAllWindows()
